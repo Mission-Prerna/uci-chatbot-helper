6cb13280e746aae530831f9fd7c91bf64e9c0d14
Ajay Kumar Maurya <ajaymaurya6798@gmail.com>
2024-05-31 09:42:21 +0530

    feat: uci v2 changes (#20)

--- src/dto/CreateSegmentBotMapping.dto.ts
+++ src/dto/CreateSegmentBotMapping.dto.ts
@@ -1,10 +1,20 @@
-import { IsInt, IsNotEmpty, IsUUID } from 'class-validator';
+import { IsInt, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { v4 as uuid } from 'uuid';
 
 export class CreateSegmentBotMappingDto {
   @IsInt()
   @IsNotEmpty()
   segmentId: bigint;
+
+  @IsUUID()
+  @IsNotEmpty()
+  botId: uuid;
+}
+
+export class CreateSegmentBotMappingDtoV2 {
+  @IsString()
+  @IsNotEmpty()
+  segmentId: string;
 
   @IsUUID()
   @IsNotEmpty()
